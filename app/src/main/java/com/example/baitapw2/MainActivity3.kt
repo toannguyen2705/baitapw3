ae4e2abcc876eca2495f52e822a42ed5dcf21edd
ToanNguyen2705 <toannguyen.27052001@gmail.com>
2023-02-18 16:37:30 +0700

    hello

---
+++ app/src/main/java/com/example/baitapw2/MainActivity3.kt
@@ -0,0 +1,19 @@
+package com.example.baitapw2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main3)
+        val button = findViewById<Button>(R.id.button2)
+        button.setOnClickListener {
+            val intent: Intent = Intent (this, MainActivity4::class.java)
+            startActivity(intent)
+
+        }
+    }
+}
